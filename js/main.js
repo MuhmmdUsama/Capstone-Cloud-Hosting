9f66f47181b46329a91200c1c9fd9dede4e96891
Muhmmd-Usama <mummdusama@gmail.com>
2022-04-18 18:17:36 +0200

    Update mobile menu to appears over the page

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
-const collapsibles = document.querySelectorAll('.collapsible');
-collapsibles.forEach((item) => item.addEventListener('click', function open() {
-  this.classList.toggle('collapsible--expanded');
-  this.classList.toggle('change');
+const hamburger = document.querySelector('.hamburger');
+
+const navMenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+});
+
+document.querySelectorAll('.nav-item').forEach((e) => e.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
 }));
 
 // ############## Owners Section ####################
